47c67f28fa2cef6c91cbb911a1c3cc9120a509ed
dalton salmonjr <daltons09@gmail.com>
2019-02-20 01:53:05 -0500

    audio playback

--- src/components/Album.js
+++ src/components/Album.js
@@ -6,13 +6,44 @@ class Album extends Component {
 		  super(props);
 		
 		const album = albumData.find( album => {
-		       return album.slug === this.props.match.params.slug
-		     });
+		  return album.slug === this.props.match.params.slug
+		});
 		 
-		     this.state = {
-		       album: album
-		     };
+		this.state = {
+		  album: album,
+			currentSong: album.songs[0],
+			isPlaying: false
+		};
+		
+		this.audioElement = document.createElement('audio');
+		this.audioElement.src = album.songs[0].audioSrc;
 		}
+		
+		play() {
+			  this.audioElement.play();
+			  this.setState({ isPlaying: true });
+			}
+		 
+		pause() {
+			  this.audioElement.pause();
+			  this.setState({ isPlaying: false });
+			}   
+		 
+		setSong(song) {
+			  this.audioElement.src = song.audioSrc;
+			  this.setState({ currentSong: song });
+			}
+		 
+		handleSongClick(song) {
+			  const isSameSong = this.state.currentSong === song;
+			  if (this.state.isPlaying && isSameSong) {
+			       this.pause();
+			     } else {
+				   if (!isSameSong) { this.setSong(song); } 	
+			       this.play();
+			     }	
+			}
+		 		
 	
 	render() {
 	  return (
